a9b803882f00583822ad0cfea0f114b6a418db5f
Kollen Gruizenga <64032109+Awes35@users.noreply.github.com>
2021-10-13 21:56:49 -0400

    Delete insertionSelectionSorts directory

--- insertionSelectionSorts/arrayUtil.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-#ifndef ARRAYUTIL_H
-#define ARRAYUTIL_H
-void printArray(int arr[], int sz);
-#endif
--- insertionSelectionSorts/sort.h
+++
@@ -1,5 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-void selectionSort(int arr[], int sz);
-void insertionSort(int arr[], int sz);
-#endif
